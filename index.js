4d7a396cec9881ab1431bd38f10bfbf16b9be89c
md-tahmid-hasan-golap <golapraj47@gmail.com>
2025-11-09 12:56:44 +0600

    adda collontion

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const port = process.env.PORT || 5000;
 
 
 
+// middel ware
 app.use(cors());
 app.use(express.json());
 
@@ -38,6 +39,19 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+
+    // all collaction
+    const productCollaction = client.db("productDB").collection("products")
+
+
+    app.post('/products', async(req, res) => {
+        const newProduct = req.body;
+        const result = await productCollaction.insertOne(newProduct);
+        res.send(result);
+    })
+    
+
 
 
 
@@ -88,9 +102,10 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('🌍 Import Export Hub Server is Running Successfully!');
+});
+
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`✅ Import Export Hub Server is Running on Port: ${port}`);
+});
